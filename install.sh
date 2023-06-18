0585fa60312b52ba10741fbef54491665a8942b7
Steve Molloy <smolloy@hotmail.com>
2023-06-18 15:26:07 -0700

    Update post server address for sending automation events.

--- install.sh
+++ install.sh
@@ -71,7 +71,7 @@ sudo chmod a+rw /opt/${HOME_NET_NAME}/snaps
 # Create and start the docker container
 export _HOME_NET_NAME="${HOME_NET_NAME}"
 export _MY_IP_ADDR=$(get_home_ip_address_for "smtp2http" && echo ${_IP_SMTP2HTTP})
-export _HOMEBRIDGE_ADDR="homebridge"
+export _HOMEBRIDGE_ADDR="homebridge:8081"
 export _PROJECT_NAME="${IMAGE_NAME}"
 docker compose -f "${SCRIPT_DIR}/docker-compose.yml" -p "${IMAGE_NAME}" up --build -d
 
